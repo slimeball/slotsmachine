6eaaf20ab12f7f200f37f4bd952a685b04cfc819
Tosakan <Tosakan@ms188.com>
2022-04-14 01:36:20 +0700

    no message

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,7 +1,7 @@
 export default {
   getUrlKey(key) {
     return decodeURIComponent((new RegExp('[?|&]' + key + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
-  },
+  }
   // getUrlKey() {
   //   return new Proxy(new URLSearchParams(window.location.search), {
   //     get: (searchParams, prop) => searchParams.get(prop),
--- src/service/axiosConfig.js
+++ src/service/axiosConfig.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import apiAddress from './apiAddress';
 import UTILS from '../assets/js/utils';
 axios.defaults.timeout = 500000;
+// axios.defaults.headers.common['Sign-Token'] = 'TpxKpHUgJJml5hFYVIhBEY7vMrmAZIYJGhE51uQ3Ad8O%2BT5kAkQ0M9yn2B3WJ%2F%2Badn%2B2jZnZydROkD2GkVnTHA%3D%3D'
 axios.defaults.headers.common['Sign-Token'] = encodeURIComponent(UTILS.getUrlKey('token'));
 const domain = apiAddress;
 
